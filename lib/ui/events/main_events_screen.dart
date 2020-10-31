a8b6e4a3ecd66660e1d8e4a517648d241cdf3fb4
Victor Sysuev <arston_sv@outlook.com>
2020-10-31 21:21:00 +0500

    [fix] main appbar

--- lib/ui/events/main_events_screen.dart
+++ lib/ui/events/main_events_screen.dart
@@ -14,80 +14,41 @@ class _MainEventsScreenState extends State<MainEventsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(150),
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 50),
+          length: 3,
+          child: Scaffold(
+            body: DefaultTabController(
+              length: 3,
+              child: Scaffold(
+                appBar: AppBar(
+                  bottom: TabBar(
+                    isScrollable: true,
+                    labelStyle: TextStyle(
+                        fontSize: 16.0,
+                        fontFamily: 'Family Name',
+                        fontWeight: FontWeight.w500), //For Selected tab
+                    tabs: [
+                      Tab(text: 'Список'),
+                      Tab(text: 'Рекомендации'),
+                      Tab(text: 'Карта'),
+                    ],
+                  ),
+                  title: Text(
+                    'События',
+                    style:
+                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.w500),
+                  ),
                 ),
-                Padding(
-                    padding: const EdgeInsets.only(left: 12, right: 12),
-                    child: Column(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(8.0),
-                                topRight: Radius.circular(8.0)),
-                            color: Color(0xffF13B5E),
-                            // color: Color(0xffE8E8E8),
-                            shape: BoxShape.rectangle,
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.only(left: 12),
-                            child: TextFormField(
-                              maxLines: 1,
-                              keyboardType: TextInputType.text,
-                              autofocus: false,
-                              decoration: InputDecoration(
-                                  hintText: 'Поиск мероприятий',
-                                  hintStyle: TextStyle(
-                                      color: Colors.black,
-                                      decoration: TextDecoration.underline,
-                                      fontSize: 20)),
-                              validator: (value) => value.isEmpty
-                                  ? 'Поле не может быть пустым'
-                                  : null,
-                            ),
-                          ),
-                        ),
-                        // Padding(padding: EdgeInsets.only(top: 12),),
-                        Container(
-                          color: Color(0xffF13B5E),
-                          child: TabBar(
-                            // isScrollable: true,
-                            labelStyle: TextStyle(
-                                fontSize: 16.0,
-                                fontFamily: 'Family Name',
-                                fontWeight: FontWeight.w500), //For Selected tab
-                            tabs: [
-                              Tab(
-                                text: 'Список',
-                              ),
-                              Tab(text: 'Рекомендации'),
-                              Tab(text: 'Карта'),
-                            ],
-                          ),
-                        )
-                      ],
-                    )),
-              ],
+                body: TabBarView(
+                  physics: NeverScrollableScrollPhysics(),
+                  children: [
+                    EventsListScreen(),
+                    RecommendsEventsListScreen(),
+                    EventsOnMapScreen(),
+                  ],
+                ),
+              ),
             ),
-          ),
-
-          body: TabBarView(
-            physics: NeverScrollableScrollPhysics(),
-            children: [
-              EventsListScreen(),
-              RecommendsEventsListScreen(),
-              EventsOnMapScreen(),
-            ],
-          ),
-        ),
-      ),
+          )),
     );
   }
 }
